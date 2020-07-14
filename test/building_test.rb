e4c31d10b1c857dee622147e59dcf7855537d342
fieldstyler <tdfields93@gmail.com>
2020-07-14 09:40:55 -0600

    Created building_can_add_units test and passes.

--- test/building_test.rb
+++ test/building_test.rb
@@ -17,4 +17,15 @@ class BuildingTest < MiniTest::Test
     assert_equal [], building.units
   end
 
+  def test_building_can_add_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    assert_equal [], building.units
+    building.add_unit(unit1)
+    assert_equal [unit1], building.units
+    building.add_unit(unit2)
+    assert_equal [unit1, unit2], building.units
+  end
+
 end
