ea7a9bf4f3c72781fb18f39ec52f0504272658d5
fieldstyler <tdfields93@gmail.com>
2020-07-14 10:52:10 -0600

    Changed add_renter method to try to help me in the Building class.

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -27,6 +27,13 @@ class Apartment
   def add_renter(renter)
     apartment_occupant = []
     apartment_occupant << renter
-    @renter = apartment_occupant[0]
+    @renter = apartment_occupant
+    #I feel like this isn't dynamic enough which is why
+    #my renters method fails in building class.
+    #Later added the following code
+    renter_names = @renter.each do |renter|
+      renter.name
+    end
+    renter_names
   end
 end
