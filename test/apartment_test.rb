ff54d10b7946ba0ae4f93caac1fc9a0222263f58
fieldstyler <tdfields93@gmail.com>
2020-07-14 09:06:58 -0600

    Created apartment_test and tested it exists.

---
+++ test/apartment_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/apartment'
+require './lib/renter'
+require "pry"
+
+class ApartmentTest < MiniTest::Test
+
+  def test_it_exists
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_instance_of Apartment, unit1
+  end
+end 
