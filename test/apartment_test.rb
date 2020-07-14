7b3858b76325f5615f0c2b4334ad6d5ed4698c83
fieldstyler <tdfields93@gmail.com>
2020-07-14 09:18:41 -0600

    Test for number of bedrooms in apartment.

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -25,4 +25,9 @@ class ApartmentTest < MiniTest::Test
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     assert_equal 1, unit1.bathrooms
   end
+
+  def test_number_of_bedrooms_in_apartment
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal 1, unit1.bedrooms
+  end
 end
