753c4e2bc995ef660d39ddffed7da33c085e1262
fieldstyler <tdfields93@gmail.com>
2020-07-14 10:28:34 -0600

    Stuck on renters method.

--- lib/building.rb
+++ lib/building.rb
@@ -1,14 +1,23 @@
 require "pry"
 class Building
 
-  attr_reader :units,
-              :renters
+  attr_reader :units
   def initialize
     @units = []
-    @renters = []
   end
 
   def add_unit(unit)
-    @units << unit
+    @units = @units << unit
+  end
+
+  def renters
+    add_unit(@units)
+    # @units = building.units
+    renter_names = @units.each do |unit|
+      unit.renter[0].name
+    end
+    binding.pry
+    renter_names
+    # add_renter(renter)
   end
 end
